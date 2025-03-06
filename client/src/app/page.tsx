dfb79fbc5e39ff909603ef8bfeca2ab29430a512
PC <nataliia.r@expletech.com>
2025-03-06 15:35:45 +0200

    added home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div> hello</div>;
+import { BlockRenderer } from "@/components/BlockRenderer";
+import { getHomePage } from "@/data/loaders";
+import { notFound } from "next/navigation";
+
+async function loader() {
+  const data = await getHomePage();
+
+  if (!data) notFound();
+  console.log(data);
+  return { ...data.data };
+}
+
+export default async function HomeRoute() {
+  const data = await loader();
+  const blocks = data?.blocks || [];
+  console.log(data);
+  // console.log(HeroSection);
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <BlockRenderer blocks={blocks} />
+    </div>
+  );
 }
